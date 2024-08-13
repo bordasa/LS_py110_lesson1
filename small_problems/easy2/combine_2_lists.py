25072efa4e7278ad137a47c6d4dbb27617056cc4
Alexei Bordas <alexei.bordas@gmail.com>
2024-08-12 19:38:24 -0700

    Completed the combine two lists problem from Easy 2 using PEDAC.

---
+++ small_problems/easy2/combine_2_lists.py
@@ -0,0 +1,47 @@
+#Problem
+    #Input: 2 lists
+    #Output: new list that contains all elements from both list arguments
+    # Rules: each element taken in alternation.
+    # both lists are non-empty
+    # both lists have the same number of elements
+
+#Examples
+
+
+#Data Structure
+# - lists
+
+#Algorithm
+    #create 1 pointer that works on both lists
+    #create an empty list
+    #iterate through the list, adding each element 1 by 1 to the new list
+    #return the new list
+
+#Code
+
+def interleave(list1, list2):
+    # list_length = len(list1)
+    # new_list = []
+
+    # idx = 0
+
+    # while idx < list_length:
+    #     new_list.append(list1[idx])
+    #     new_list.append(list2[idx])
+    #     idx += 1
+    
+    # return new_list
+
+    result = []
+
+    for idx in range(len(list1)):
+        result.extend([list1[idx], list2[idx]])
+    
+    return result
+
+
+
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+expected = [1, "a", 2, "b", 3, "c"]
+print(interleave(list1, list2) == expected)      # True
