4c200b03703a83cd6d5ecca150fb7a9ec48502b1
Alexei Bordas <alexei.bordas@gmail.com>
2024-09-03 19:39:53 -0700

    Completed the procedural version of the fibonacci numbers program from the Medium 1 problem set.

---
+++ small_problems/medium1/fibonacci_numbers_procedural.py
@@ -0,0 +1,23 @@
+def fibonacci(num):
+    if num <= 2:
+        return 1 # by definition
+    
+    fib_num_tracker = [1, 1]
+    
+    for _ in range(3, num + 1):
+        fib_num = sum(fib_num_tracker[-2:])
+        fib_num_tracker.append(fib_num)
+        
+    return fib_num_tracker[-1]
+
+
+print(fibonacci(1) == 1)
+print(fibonacci(2) == 1)
+print(fibonacci(3) == 2)
+print(fibonacci(4) == 3)
+print(fibonacci(5) == 5)
+print(fibonacci(6) == 8)
+print(fibonacci(12) == 144)
+print(fibonacci(20) == 6765)
+print(fibonacci(50) == 12586269025)
+print(fibonacci(75) == 2111485077978050)
