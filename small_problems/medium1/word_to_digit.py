fe82720e770f9c9d38ade998c7c8f1fc971e45dd
Alexei Bordas <alexei.bordas@gmail.com>
2024-09-03 18:47:33 -0700

    Completed Word to Digit from Medium problem set.

---
+++ small_problems/medium1/word_to_digit.py
@@ -0,0 +1,45 @@
+#Input: Strings
+#Output: String with number words converted to numbers
+#Rules: Assume no punctuation
+
+#Algorithm:
+    #1. Create a dictionary with keys of word numbers and values of integers
+    #2. Convert message into a list
+    #3. Iterate through list, if word is in dic, convert word
+    #4. Join the list into a string
+
+#Code:
+
+def word_to_digit(message_string):
+    message_list = message_string.split()
+    new_list = []
+
+    word_num_dict = {'zero': '0',
+                     'one': '1',
+                     'two': '2',
+                     'three': '3',
+                     'four': '4',
+                     'five': '5',
+                     'six': '6',
+                     'seven': '7',
+                     'eight': '8',
+                     'nine': '9'
+                     }
+
+    for word in message_list:
+        if word in word_num_dict.keys():
+            word = word_num_dict[word]
+        
+        new_list.append(word)
+
+    # new_list1 = [word_num_dict[word] for word in message_list if word in word_num_dict.keys() else word]
+    
+    # print(new_list1)
+    #I'm not seeing why this doesn't work...
+
+    return ' '.join(new_list)
+    
+
+#Example:
+message = 'Please call me at five five five one two three four'
+print(word_to_digit(message) == 'Please call me at 5 5 5 1 2 3 4')
