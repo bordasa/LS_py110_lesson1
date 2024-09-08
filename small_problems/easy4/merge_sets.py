287d3c31248d39c681f16c0d8fbbe18fc9a5b34c
Alexei Bordas <alexei.bordas@gmail.com>
2024-09-08 00:28:11 -0700

    Tried a new solution for this simple problem.

--- small_problems/easy4/merge_sets.py
+++ small_problems/easy4/merge_sets.py
@@ -1,8 +1,9 @@
 def merge_sets(l1, l2):
-    set1 = set(l1)
-    set2 = set(l2)
+    # set1 = set(l1)
+    # set2 = set(l2)
 
-    return set1.union(set2)
+    # return set1.union(set2)
+    return set(l1).union(set(l2))
 
 list1 = [3, 5, 7, 9]
 list2 = [5, 7, 11, 13]
