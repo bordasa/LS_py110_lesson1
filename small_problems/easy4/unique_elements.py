9709b002777f3d4212436d691639a165a042023b
Alexei Bordas <alexei.bordas@gmail.com>
2024-09-08 22:27:01 -0700

    Completed the Unique Elements problem from the Easy 4 problem set.

---
+++ small_problems/easy4/unique_elements.py
@@ -0,0 +1,13 @@
+# def unique_from_first(list1, list2):
+#     return {num for num in list1 if num not in list2}
+
+# list1 = [3, 6, 9, 12]
+# list2 = [6, 12, 15, 18]
+# print(unique_from_first(list1, list2) == {9, 3})
+
+def unique_from_first(list1, list2):
+    return set(list1) - set(list2)
+
+list1 = [3, 6, 9, 12]
+list2 = [6, 12, 15, 18]
+print(unique_from_first(list1, list2) == {9, 3})
