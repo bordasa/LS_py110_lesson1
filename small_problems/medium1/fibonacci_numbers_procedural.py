be25b89a101ab5628e19947b05e5a79148bc179e
Alexei Bordas <alexei.bordas@gmail.com>
2024-09-09 13:25:48 -0700

    Resolved the problem in a more memory-efficient way based on how the LS solution presents it.

--- small_problems/medium1/fibonacci_numbers_procedural.py
+++ small_problems/medium1/fibonacci_numbers_procedural.py
@@ -1,15 +1,25 @@
-def fibonacci(num):
-    if num <= 2:
-        return 1 # by definition
+# def fibonacci(num):
+#     if num <= 2:
+#         return 1 # by definition
     
-    fib_num_tracker = [1, 1]
+    # fib_num_tracker = [1, 1]
     
-    for _ in range(3, num + 1):
-        fib_num = sum(fib_num_tracker[-2:])
-        fib_num_tracker.append(fib_num)
+    # for _ in range(3, num + 1):
+    #     fib_num = sum(fib_num_tracker[-2:])
+    #     fib_num_tracker.append(fib_num)
         
-    return fib_num_tracker[-1]
+    # return fib_num_tracker[-1]
+    
+def fibonacci(nth):
+    if nth <= 2:
+        return 1 #by definition
+    
+    previous, current = 1, 1
 
+    for _ in range(3, nth + 1):
+        previous, current = current, (previous + current)
+    
+    return current
 
 print(fibonacci(1) == 1)
 print(fibonacci(2) == 1)
